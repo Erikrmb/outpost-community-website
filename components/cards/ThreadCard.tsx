6d95821fee2481bca1b6d84842135aa9671c5b31
Erikrmb <erikrmb@gmail.com>
2023-10-10 23:33:39 -0400

    Creating post card

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,68 @@
+import Link from "next/link";
+import Image from "next/image";
+
+interface Props{
+    id: string;
+    currentUserId: string;
+    parentId: string | null;
+    content: string;
+    author: {
+        name: string;
+        image: string;
+        id: string;
+    }
+    community: {
+        id: string;
+        name: string;
+        image: string;
+    } | null;
+    createdAt: string;
+    comments: {
+        author: {
+            image: string;
+
+        }
+    }[]
+    isComment?: boolean;
+
+}
+
+export default function ThreadCard({
+    id,
+    currentUserId,
+    parentId,
+    content,
+    author,
+    community,
+    createdAt,
+    comments
+}: Props){
+    return(
+        <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+            <div className="flex items-start justify-between">
+                <div className="flex w-full flex-1 flex-row gap-4">
+                    <div className="flex flex-col items-center">
+                        <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+                            <Image 
+                                src={author.image}
+                                alt="Profile image"
+                                fill
+                                className="cursor-pointer rounded-full"
+                            />
+                        </Link>
+                        <div className="thread-card_bar"/>
+                    </div>
+
+                    <div>
+                        <Link></Link>
+                    </div>
+
+                </div>
+            </div>
+
+            <h2 className="text-small-regular text-light-2">
+                {content}
+            </h2>
+        </article>
+    )
+}
