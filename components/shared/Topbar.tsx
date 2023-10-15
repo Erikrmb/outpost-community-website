da2e175380794d509843840c5709a5fc3b0f8b00
Erikrmb <erikrmb@gmail.com>
2023-10-15 00:23:31 -0400

    Changing icon paths to work on vercel

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -17,7 +17,7 @@ export default function TopBar(){
                         <SignOutButton>
                             <div className="flex cursor-pointer">
                                 <Image
-                                    src="/assets/logout.svg"
+                                    src="Assets/logout.svg"
                                     alt="logout"
                                     width={24}
                                     height={24}
